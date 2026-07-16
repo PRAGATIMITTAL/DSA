ef09ae0e025fcc53f47b2b7aa908be202622da07
PRAGATIMITTAL <93858683+PRAGATIMITTAL@users.noreply.github.com>
2026-07-16 23:37:27 +0530

    Time: 0 ms (100%), Space: 43.1 MB (97.58%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,5 +1,26 @@
 class Solution {
     public void sortColors(int[] nums) {
-        Arrays.sort(nums);
+        int i=0;
+        int m = 0;
+        int h = nums.length-1;
+        while(m<=h){
+            if(nums[m]==0) {
+                int temp = nums[i];
+                nums[i] = nums[m];
+                nums[m] = temp;
+                m++;
+                i++;
+            } 
+            else if(nums[m]==1){
+                m++;
+            }
+            else {
+                int temp = nums[m];
+                nums[m] = nums[h];
+                nums[h]= temp;
+                h--;
+            }
+            
+        }
     }
 }
