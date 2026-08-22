2984d3eb93fcff15cc527a162fe9fe8550373353
PRAGATIMITTAL <93858683+PRAGATIMITTAL@users.noreply.github.com>
2026-08-22 16:12:03 +0530

    Time: 1 ms (100%), Space: 119.2 MB (33.42%) - LeetHub

--- 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.java
+++ 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.java
@@ -10,33 +10,20 @@
  */
 class Solution {
     public ListNode swapNodes(ListNode head, int k) {
-         ListNode fast = head;
-        ListNode slow = head;
-        ListNode first = head, second = head;
-        
-		// Put fast (k-1) nodes after slow
-        for(int i = 0; i < k - 1; ++i)
-            fast = fast.next;
-            
-		// Save the node for swapping
-        first = fast;
-
-		// Move until the end of the list
-        while(fast.next != null) {
-			slow = slow.next;
-            fast = fast.next;
+        ListNode first=head;
+        ListNode fast=head;
+        for(int i=1; i<k; i++){
+            first=first.next;
         }
-        
-        // Save the second node for swapping
-		// Note that the pointer second isn't necessary: we could use slow for swapping as well
-		// However, having second improves readability
-        second = slow;
-		
-		// Swap values
+        fast= first;
+        ListNode second = head;
+        while(fast.next!=null){
+            fast=fast.next;
+            second=second.next;
+        }
         int temp = first.val;
-        first.val = second.val;
+        first.val= second.val;
         second.val = temp;
-        
         return head;
     }
 }
